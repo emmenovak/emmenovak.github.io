5a6a4ae03c759f4b7987e68caf3a25d45f6c3879
emmenovak <emno1506@colorado.edu>
2023-11-28 10:41:50 -0700

    wa13 UPDATE

---
+++ wa/wa13/wa13.js
@@ -0,0 +1,38 @@
+// Assignment 13: Human Resources
+
+// Q1
+employees = {
+{
+    "first_name";"Sam",
+    "department"; "Tech",
+    "designation"; "Manager",
+    "salary"; "40000",
+    "raise_eligible"; "true"
+}
+{
+    "first_name";"Mary",
+    "department"; "Finance",
+    "designation"; "Trainee",
+    "salary"; "18500",
+    "raise_eligible"; "true"
+}
+{
+    "first_name";"Bill",
+    "department"; "HR",
+    "designation"; "Executive",
+    "salary"; "21200",
+    "raise_eligible"; "false"
+}
+    
+
+};
+
+// Q2
+
+// Q3
+
+// Q4
+
+// Q5
+
+// Q6
